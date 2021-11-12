518a6e7378053a42f5e29044642186173c4867f4
UpBeatPredator <77713906+hackerboi72@users.noreply.github.com>
2021-11-12 15:37:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+function whatisgonnahappenoooo() {
+    Webcam.snap(
+        function (result_image) {
+            document.getElementById("result").innerHTML = '<img id = "captured_image" src = "' + result_image + '">';
+
+        }
+    )
+}
+function identifyimage () {
+    
+}
+Webcam.set({
+    width: 400,
+    height: 300,
+    image_format: 'jpg', 
+    jpg_quality: 90
+});
+
+camera = document.getElementById("camera");
+Webcam.attach("#camera")
+console.log("ml5version: " , ml5.version); 
+
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/V9ENMcfVT/model.json", modelloaded)
+function modelloaded () {
+    console.log("model is loaded ");
+    
+}
+function check() {
+     img = document.getElementById("captured_image"); 
+     classifier.classify(img, gotresult) 
+}
+function gotresult(error, result) {
+    if(error) {
+        console.error(error)
+    }else{
+        console.log(result)
+        document.getElementById("object_name").innerHTML = result[0].label 
+        document.getElementById("object_accuracy").innerHTML = result[0].confidence.toFixed(3)
+    }
+}
